294995b7a8cb48e74a76c0728fc87b89cb9248f4
Kenneth  Jao <ksjdragon@gmail.com>
2015-01-11 17:55:33 -0500

    Added working chromebooks

--- client/teacher.js
+++ client/teacher.js
@@ -17,3 +17,32 @@ Template.teacher.helpers({
    		 ];
 	}
 });
+
+Template.teacher.helpers({
+  chromebooks: function() {
+    return Chromebooks.find();
+  }
+});
+
+
+Template.teacher.events({
+  'click .cross' : function() {
+    if (Roles.userIsInRole(Meteor.userId(), ['admin'])) {
+      if (this.status === 0) {
+        Chromebooks.update(this._id, {$set: {status: 2}});
+        Chromebooks.update(this._id, {$set: {user: null}});
+      }
+      else if (this.status ===1) {
+        Chromebooks.update(this._id, {$set: {status: 2}});
+      }
+      else {
+        Chromebooks.update(this._id, {$set: {status: 0}});
+        Chromebooks.update(this._id, {$set: {last_checkout: null}});
+        Chromebooks.update(this._id, {$set: {userid: null}});
+      }
+    }
+    else {
+      alert("Access Denied");
+    }
+  }
+});
