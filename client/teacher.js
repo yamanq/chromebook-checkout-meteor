72ed485e15672fd5f2f24306b8d1891dbea60b94
Kenneth  Jao <ksjdragon@gmail.com>
2015-01-11 19:00:59 -0500

    stuffs

--- client/teacher.js
+++ client/teacher.js
@@ -1,10 +1,6 @@
 ReactiveTabs.createInterface({
   template: 'teacherTabs',
   onChange: function (slug, template) {
-    // This callback runs every time a tab changes.
-    // The `template` instance is unique per {{#basicTabs}} block.
-    console.log('[tabs] Tab has changed! Current tab:', slug);
-    console.log('[tabs] Template instance calling onChange:', template);
   }
 });
 
