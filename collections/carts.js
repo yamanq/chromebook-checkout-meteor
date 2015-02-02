7bff80d54c04f48a80dd968a7e51bbc2fe7b8bca
yamanq <ybq987@gmail.com>
2015-02-01 23:19:54 -0500

    added security and fixed bugs in previous push

--- collections/carts.js
+++ collections/carts.js
@@ -1,1 +1,9 @@
 carts = new Mongo.Collection("carts");
+carts.allow({
+  insert: function (userId, doc) {
+    return Roles.userIsInRole(Meteor.user()._id, ['admin']);
+  },
+  remove: function (userId, doc) {
+    return Roles.userIsInRole(Meteor.user()._id, ['admin']);
+  }
+});
