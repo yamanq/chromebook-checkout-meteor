4650bf15304af25295e812b6f04cf61c71fc628c
yamanq <ybq987@gmail.com>
2015-02-01 23:09:03 -0500

    allowed chromebooks to be added only by admin

--- collections/chromebooks.js
+++ collections/chromebooks.js
@@ -1,1 +1,9 @@
 Chromebooks = new Mongo.Collection("chromebook");
+Chromebooks.allow({
+  insert: function (userId, doc) {
+    return Roles.userIsInRole(Meteor.user()._id, ['admin']);
+  },
+  remove: function (userId, doc) {
+    return Roles.userIsInRole(Meteor.user()._id, ['admin']);
+  }
+});
